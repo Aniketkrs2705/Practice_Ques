e231ce03f248e7e225eeced19bfb1ac085fe4675
Aniketkrs2705 <Aniketkumar.work27@gmail.com>
2024-10-17 09:15:32 +0530

    First Commit

--- addTwoNo.java
+++ addTwoNo.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class addTwoNo
+{
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int sum = a+b+c;
+
+        System.out.println("Sum is : " + sum);
+
+        
+    }
+}
